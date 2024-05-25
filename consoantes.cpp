db4d4dc6aa24313e1296d34759aeede2d190a465
TauaRibeiro <166765664+TauaRibeiro@users.noreply.github.com>
2024-05-25 18:48:39 -0300

    Update consoantes.cpp

--- consoantes.cpp
+++ consoantes.cpp
@@ -5,48 +5,6 @@
 
 void aloca(int tamanho, char *ptr);
 
-int main(){
-    system("cls");
-    setlocale(LC_ALL, "portuguese");
-
-    char *ptr = (char *)malloc(sizeof(char)), vogais[] = "aáàâãóôõoeéêuúií";
-    int tamanho, vogal;
-
-    printf("Digite o tamanho da string: ");
-    scanf("%d", &tamanho);
-
-    aloca(tamanho, ptr);
-
-    printf("Digite a string: ");
-    scanf(" %[^\n]", ptr);
-
-    printf("String sem vogais: ");
-    for(int i = 0; i < tamanho; i++){
-        vogal = 0;
-        
-        for(int x = 0; x < 16; x++){
-            if(tolower(ptr[i]) == vogais[x]){
-                vogal = 1;
-            }
-        }
-
-        if(!(vogal)){
-            printf("%c", ptr[i]);
-        }
-    }
-    return 0;
-}
-
-void aloca(int tamanho, char *ptr){
-    ptr = (char *)realloc(ptr, tamanho*sizeof(char));
-}
-#include <stdio.h>
-#include <stdlib.h>
-#include <locale.h>
-#include <ctype.h>
-
-void aloca(int tamanho, char *ptr);
-
 
 int main(){
     system("cls");
@@ -65,7 +23,7 @@ int main(){
     scanf(" %[^\n]", ptr);
 
     printf("String sem vogais: ");
-    for(int i = 0; i < tamanho; i++){
+    for(int i = 0; ptr[i] != '\0'; i++){
         vogal = 0;
 
         for(int x = 0; x < 28; x++){
@@ -78,6 +36,8 @@ int main(){
             printf("%c", ptr[i]);
         }
     }
+
+    free(*ptr);
     return 0;
 }
 
