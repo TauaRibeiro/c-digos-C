ff5ba6099b550f0d0736e70361bd021a8360d0d8
TauaRibeiro <166765664+TauaRibeiro@users.noreply.github.com>
2024-05-25 18:42:40 -0300

    Update consoantes.cpp

--- consoantes.cpp
+++ consoantes.cpp
@@ -40,3 +40,47 @@ int main(){
 void aloca(int tamanho, char *ptr){
     ptr = (char *)realloc(ptr, tamanho*sizeof(char));
 }
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <ctype.h>
+
+void aloca(int tamanho, char *ptr);
+
+
+int main(){
+    system("cls");
+    setlocale(LC_ALL, "portuguese");
+
+    char *ptr = (char *)malloc(sizeof(char));
+    char vogais[] = "aeiou";
+    int tamanho, vogal;
+
+    printf("Digite o tamanho da string: ");
+    scanf("%d", &tamanho);
+
+    aloca(tamanho, ptr);
+
+    printf("Digite a string: ");
+    scanf(" %[^\n]", ptr);
+
+    printf("String sem vogais: ");
+    for(int i = 0; i < tamanho; i++){
+        vogal = 0;
+
+        for(int x = 0; x < 28; x++){
+            if(tolower(ptr[i]) == vogais[x]){
+                vogal = 1;
+            }
+        }
+
+        if(!(vogal)){
+            printf("%c", ptr[i]);
+        }
+    }
+    return 0;
+}
+
+void aloca(int tamanho, char *ptr){
+    ptr = (char *)realloc(ptr, tamanho*sizeof(char));
+}
