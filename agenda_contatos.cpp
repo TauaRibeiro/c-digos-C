98b6c709676b1c783c8759ed3abd4f71182f89d0
TauaRibeiro <166765664+TauaRibeiro@users.noreply.github.com>
2024-06-04 14:52:44 -0300

    Create agenda_contatos.cpp (em construção)

---
+++ agenda_contatos.cpp
@@ -0,0 +1,170 @@
+//Ainda em construção
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <locale.h>
+#include <time.h>
+#include <string.h>
+
+struct agenda{
+    int mes, ano;
+    char telefone[15], nome[];
+};
+
+void cadastro(FILE **a, struct agenda **c, int indice);
+void remocao(FILE **a, struct arquivo[]);
+void linha(const char *tipo, int tamanho);
+
+int main(){
+    system("cls");
+    setlocale(LC_ALL, "Portuguese");
+
+    FILE *arquivo;
+    struct agenda *contato;
+    int decisao, qtd_contatos;
+    int fim = 0;
+
+    arquivo = fopen("gravacao.txt", "r");
+    
+    if(arquivo == NULL){
+        fclose(arquivo);
+
+        arquivo = fopen("gravacao.txt", "w");
+        
+        fprintf(arquivo, "%d", 1);
+
+        contato = (struct agenda *)malloc(sizeof(struct agenda));
+    }
+    else{
+        fscanf(arquivo, "%d", &qtd_contatos);
+
+        contato = (struct agenda *)malloc(qtd_contatos*sizeof(struct agenda));
+    }
+
+    fclose(arquivo);
+
+    do{
+        do{
+            linha("=-", 25);
+            printf("ESCOLHA UMA DAS OPÇÕES ABAIXO:");
+            linha("-", 30);
+            printf("1- Inserir contato;\n2- Remover contato;\n3- Pesquisar contato;\n");
+            printf("4- Listar contatos;\n5- Imprimir aniversário do mês;");
+            linha("-", 30);
+            printf("Digite o número da opção desejada: ");
+            scanf("%d", &decisao);
+
+            if(decisao <= 5 && decisao >= 1){
+                linha("=-", 25);
+
+                break;
+            }
+
+            system("cls");
+
+            printf("Escolha inválida!!! Tente novamente;");
+        }while(1);
+        
+        switch(decisao){
+            case 1:
+                cadastro(&arquivo, &contato, qtd_contatos-1);
+
+                printf("Cadastro realizado com sucesso!!");
+            break;
+        }
+    }while(fim);
+
+    return 0;
+}
+
+void linha(const char *tipo, int tamanho){
+    printf("%c", '\n');
+
+    for(int i = 0; i < tamanho; i++){
+        printf(tipo);
+    }
+
+    printf("%c", '\n');
+}
+
+void cadastro(FILE **a, struct agenda **c, int indice){
+    int veri_data(int mes, int ano);
+
+    char nome[100], telefone[15];
+    int mes, ano;
+    int achou_erro, erro_telefone, erro_data;
+
+    (*a) = fopen("gravacao.txt", "w");
+
+    do{
+        achou_erro = erro_data = erro_telefone = 0;
+
+        linha("-", 30);
+        printf("Digite o nome da pessoa: ");
+        fscanf(stdin ," %99[^\n]", nome);
+
+        fflush(stdin);
+        
+        linha("-", 30);
+        printf("Digite o número de telefone de %s (+55xxxxxxxxxxx): ", nome);
+        fscanf(stdin, " %14[^\n]", telefone);
+
+        fflush(stdin);
+
+        linha("-", 30);
+        printf("Digite o mês e ano qm que %s faz aniversário (mm/aaaa): ", nome);
+        scanf("%2d/%d", &mes, &ano);
+
+        system("cls");
+        
+        if(!veri_data(mes, ano)){
+            achou_erro = erro_data = 1;
+        }
+        if(!(strlen(telefone) == 14)){
+            achou_erro = erro_telefone = 1;
+        }
+
+        if(!achou_erro){
+            break;
+        }
+
+        printf("ERRO!!\n");
+        printf("%s",(erro_data) ? "\nData de aniversário inválida..." : "");
+        printf("%s", (erro_telefone) ? "\nNúmero de telefone inválido..." : "");
+
+    }while(1);
+
+    (*c)[indice].ano = ano;
+    (*c)[indice].mes = mes;
+    strcpy((*c)[indice].nome, nome);
+    strcpy((*c)[indice].telefone, telefone);
+
+    (*c) = (struct agenda *)realloc((*c), (indice+2)*sizeof(struct agenda));
+
+    fprintf((*a), "%d\n", indice+2);
+
+    for(int i = 0; i <= indice; i++){
+        fprintf((*a), "NOME: %s\nTELEFONE: %s\n", nome, telefone);
+        fprintf((*a), "DATA ANIVERSÁRIO: %d/%d\n", mes, ano);
+    }
+    
+
+    fclose((*a));
+}
+
+int veri_data(int mes, int ano){
+    time_t t;
+    time(&t);
+    struct tm *tempo = localtime(&t);
+
+    int ano_atual = tempo->tm_year + 1900;
+
+    if(mes > 12 || mes < 1){
+        return 0;
+    }
+    if(ano > ano_atual || ano <= 1824){
+        return 0;
+    }
+
+    return 1;   
+}
